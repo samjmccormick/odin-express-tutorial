9364b73ae05c55160fa5b3907659d4d5871b63cf
samjmccormick <samjmccormick@gmail.com>
2025-01-05 12:38:19 -0800

    added views, controllers, and ejs lint

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+const year = new Date().getFullYear();
+
+const company = "Odin Project";
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    links: links,
+    users: users,
+    year: year,
+    company: company,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    links: links,
+    company: company,
+    year: year,
+  });
+});
 
 app.use((err, req, res, next) => {
   console.error(err);
